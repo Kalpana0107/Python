f487c304b67f5f45432908e25df0fd84dcc03a26
Kalpana0107 <naikodikalpana07@gmail.com>
2026-07-10 20:10:00 +0530

    Modiied Conditionals

--- Basics/Conditionals.py
+++ Basics/Conditionals.py
@@ -1,22 +1,41 @@
-# x=int(input("Enter here number: "))
+x=int(input("Enter here number: "))
 
 # # No additional treatment for edge cases
-# if(x%2 == 0):
-#     print("X is Even.")
-# else:
-#     print("X is Odd")
+if(x%2 == 0):
+    print("X is Even.")
+else:
+    print("X is Odd")
 
 # Using try catch
 while(True):
     try:
         num= int(input("Enter here a number: "))
         if(num%2 == 0):
-            print("X is Even.")
+            print("Number is Even.")
         else:
-            print("X is Odd")
+            print("Number is Odd")
         break
         
     except :
-        print("Please Enter Valid Input !")
+        print("Please Enter Valid Input (Integer) !")
         pass
-        
+
+name=input("Enter here your name: ")
+
+# Using if - elif - else
+if(name == 'Harry' ):
+    print("Gryffindor")
+elif(name== 'Draco'):
+    print("Slytherin")
+else:
+    print("Invalid name.")
+
+# Using Match
+match name:
+    case 'Harry':
+        print("Gryffindor")
+    case 'Draco':
+        print("Slyrtherin")
+    case _:
+        print("Invalid Input")
+
