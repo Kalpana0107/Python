49ef7f82bcf44038eb686fde3b71121be7e44344
Kalpana0107 <naikodikalpana07@gmail.com>
2026-07-19 11:47:57 +0530

    Revision added

---
+++ Basics/Revision/hello.py
@@ -0,0 +1,12 @@
+def main():
+    print(hellow("David"))
+
+def hellow(name="world"):
+    return f"hello,{name}"
+
+def sum(x,y):
+    return x+y
+
+if __name__=="__main__":
+    main()
+
---
+++ Basics/Revision/test2.py
@@ -0,0 +1,7 @@
+from hello import sum
+import pytest
+
+def test_sum():
+    assert sum(3,4)==7
+    
+        
